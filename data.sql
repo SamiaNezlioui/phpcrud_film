370a533aa0278989eb220bbb86f1241f6e9d425a
SamiaNezlioui <84906495+SamiaNezlioui@users.noreply.github.com>
2021-07-28 14:37:25 +0200

    page ajouter avec image

---
+++ data.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE film (
+id int primary key auto_increment,
+titre varchar(50),
+annee date,
+image varchar(50)
+);
+
+/*Ajout des films*/
+insert into film (titre, annee, image) values
+("l'etrange noel de mr Jack", "1993-12-24", "/images/noel.jpg" ),
+("cobra", "1986-06-10", "/images/cobra.jpg" ),
+("interstellar", "2014-11-07", "/images/interstellar.jpg" );
+
+SELECT * FROM film;
+
+/*editer les films*/
+UPDATE film set titre = "interstelar"  where id=3;
