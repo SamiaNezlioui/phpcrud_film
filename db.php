68e24f8379d77fcb642a026d65b3543c1e9faa46
SamiaNezlioui <84906495+SamiaNezlioui@users.noreply.github.com>
2021-07-28 10:35:39 +0200

    ajout de la connexion a la base donnée

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$dsn = "mysql:host = localhost; dbname = cinema;";
+$username = "root";
+$password = "";
+$option= [];
+$connection = new PDO($dsn, $username, $password, $option);
+
+//fonction d'exeception on cas ya une erreur TRY
+try{
+    print "connexion reuissi";
+}catch(PDOException $e){
+    echo $e->getMessage();
+    die();
+}
+?>
